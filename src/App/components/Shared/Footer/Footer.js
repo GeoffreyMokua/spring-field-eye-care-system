00a4681a3cf34d5bf2ba662d844232f00038c2cc
GeoffreyMokua <geoffrey.nyakundi@student.moringaschool.com>
2023-01-21 14:58:21 +0300

    added the footer section

--- src/App/components/Shared/Footer/Footer.js
+++ src/App/components/Shared/Footer/Footer.js
@@ -1,8 +1,38 @@
 import React from 'react'
+import { Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import './Footer.css';
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <div className="footer-all w-100 p-3">
+            <div className="footer row ">
+            <div className="col-lg-3 col-xs-12 my-2">
+                <h2 style={{fontFamily: 'cursive'}}>Spring Field Eye Care</h2>
+                <p>A 2 year journey on eye care!</p>
+                
+            </div> 
+            <div className="col-lg-3 col-xs-12 my-4 ">
+                <p>If you feel dusty on your eyes</p>
+                <Link to="/booking"><Button variant="success">Book Appointment</Button></Link>
+            </div>
+            <div className="col-lg-3 col-xs-12 my-2">
+                <h3>Quick Visit</h3>
+                <p><Link className="url"  to="/home">Home   </Link></p>
+                <p><Link className="url"  to="/about">About </Link></p>
+                
+
+            </div> 
+            <div className="col-lg-3 col-xs-12 my-2">
+                <h3>Contact Us</h3>
+                <p>admin@springfield.com</p>
+                <p>+254721243247</p>
+            </div>
+        </div>
+        <div>
+            <footer>© Copyright 2022 by Spring Field Eye Care</footer>
+        </div>
+        </div>
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App'
-import { BrowserRouter } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
