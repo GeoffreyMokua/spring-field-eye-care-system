cba240c0f8555cd863648ab49e58fdf862c56339
GeoffreyMokua <geoffrey.nyakundi@student.moringaschool.com>
2023-01-21 16:12:37 +0300

    added booking services

--- src/App/components/LandingPage/Booking/Booking.js
+++ src/App/components/LandingPage/Booking/Booking.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+import './Booking.css'
 
 function Booking() {
   return (
-    <div>Booking</div>
+   <div className="booking row mx-3">
+            <div className="col-lg-7 col-12">
+                <p className="title">Are you looking for an eye-care Consultation?</p>
+            </div> 
+            <div className="col-lg-5 col-12">
+                <Link to="/booking">
+                  <button className="button-app">Book Appointment today <i class="fas fa-arrow-alt-circle-right"></i></button>
+                </Link>
+            </div>
+        </div>
   )
 }
 
--- src/App/components/LandingPage/Home/Home.js
+++ src/App/components/LandingPage/Home/Home.js
@@ -1,13 +1,16 @@
 import React from 'react'
+import Services from '../../Services/Services';
 import Banner from '../Banner/Banner'
 import Benefits from '../Benefits/Benefits'
 import Booking from '../Booking/Booking'
 function Home() {
   return (
     <div>
-      <Banner />
-      <Benefits />
-      <Booking />
+      
+            <Banner></Banner> 
+            <Services></Services>
+            <Booking></Booking> 
+            <Benefits></Benefits>
     </div>
   )
 }
