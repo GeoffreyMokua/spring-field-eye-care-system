c732223c8101120826073e888b8cc2d177c6c096
GeoffreyMokua <geoffrey.nyakundi@student.moringaschool.com>
2023-01-21 15:38:34 +0300

    added banners on the home page

--- src/App/components/LandingPage/Banner/Banner.js
+++ src/App/components/LandingPage/Banner/Banner.js
@@ -1,8 +1,46 @@
 import React from 'react'
+import { Carousel } from 'react-bootstrap';
+import banner1 from '../images/banners/1.png';
+import banner2 from '../images/banners/2.png'
+import banner3 from '../images/banners/3.png'
 
 function Banner() {
   return (
-    <div>Banner</div>
+     <Carousel  variant="dark" >
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src={banner1}
+      alt="First slide"
+    />
+    <Carousel.Caption>
+      <h5>Welcome To Mahmuda Eye Care</h5> 
+      
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src={banner2}
+      alt="Second slide"
+    />
+    <Carousel.Caption>
+      <h5>We Care Our Patient</h5>
+      
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src={banner3}
+      alt="Third slide"
+    />
+    <Carousel.Caption>
+       <h5 className="text-white">Always Near on Your Service</h5>
+      
+    </Carousel.Caption>
+  </Carousel.Item>
+</Carousel>
   )
 }
 
--- src/App/components/Shared/Header/Header.js
+++ src/App/components/Shared/Header/Header.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Button, Container, Nav, Navbar } from 'react-bootstrap';
+import { Container, Nav, Navbar } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import './Header.css'
 
