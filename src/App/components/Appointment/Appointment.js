738983a6270fbe8e9e9f270c691f16e1653c7a88
GeoffreyMokua <geoffrey.nyakundi@student.moringaschool.com>
2023-01-21 18:33:56 +0300

    added appointment card

--- src/App/components/Appointment/Appointment.js
+++ src/App/components/Appointment/Appointment.js
@@ -1,8 +1,52 @@
 import React from 'react'
+import { useNavigate  } from 'react-router';
+import './Appointment.css'
 
 function Appointment() {
+  const navigate = useNavigate ();
+
+     const success = () =>{
+         navigate.push('/booking/success')
+     }
   return (
-    <div>Appointment</div>
+     <div className="form">
+           <h2 className="text-success fw-bold fs-1 py-3">Book Your Appointment Now</h2>
+           <form className="my-5 " onSubmit={success}>
+               
+               <div className="mb-3">
+                    <label className="mx-2 text-white fw-bold" htmlFor="name">Name</label>
+                    <input className="mx-2 w-25" type="text" name="name" placeholder="First Name" required />
+                    <input className="mx-2 w-25" type="text" name="name" id="name "placeholder="Last Name" required />
+                    
+               </div>  
+               
+               <div className="mb-3">
+                   <label className="mx-2 text-white fw-bold" htmlFor="email">Email</label>
+                   <input required className="w-25" type="email" name="" id="" placeholder="email" />
+               </div>
+               <div className="mb-3">
+                   <label htmlFor="age" className="mx-2 text-white fw-bold">Age</label>
+                   <input type="text" name="" id="" placeholder="age" />
+               </div>
+               
+               <div className="mb-3">
+               <label className="mx-2  text-white fw-bold" htmlFor="gender">Gender</label>
+               <input type="radio" id="male" name="gender" value="Male"/>
+               <label className="mx-2 text-white fw-bold" for="male ">Male</label>
+               <input  type="radio" id="female" name="gender" value="Female"/>
+               <label  className="fw-bold text-white" for="female">Female</label> 
+               </div>
+               <div className="mb-3">
+                   <label className="mx-2 text-white fw-bold" htmlFor="date">Choose Date</label>
+                   <input required type="date" name="" id="" />
+               </div> 
+               <div>
+                   <label  className="mx-2 text-white fw-bold" htmlFor="time">Pick Time</label>
+                   <input required type="time" name="" id="" /> <br /> <br />
+               </div> 
+                <input style={{marginLeft:'6%',backgroundColor: 'yellow', border:'1px solid yellow',borderRadius:'5px'}} type="submit" value="Book Now" />
+           </form>
+        </div>
   )
 }
 
--- src/App/components/Appointment/Successfull.js
+++ src/App/components/Appointment/Successfull.js
@@ -2,7 +2,12 @@ import React from 'react'
 
 function Successfull() {
   return (
-    <div>Successfull</div>
+   <div className="mt-5 mb-5">
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQgfsd-_Hgyq8gDXB24k6vPfrKJsh53dQhrUcQCHGbLmICAO_4bnUbDHP8cdDdzS43zVFY&usqp=CAU" alt="" />
+
+            <h2 className="mt-3">We have Recieved your Booking.</h2>
+            <p style={{marginBottom:'100px'}}>Confirmation email has been sent!</p>
+        </div>
   )
 }
 
