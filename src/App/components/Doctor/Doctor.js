2613490b9d6dbd12b5a4cc27a2b6f883e5059d9a
GeoffreyMokua <geoffrey.nyakundi@student.moringaschool.com>
2023-01-22 09:46:38 +0300

    added doctors section

--- src/App/components/Doctor/Doctor.js
+++ src/App/components/Doctor/Doctor.js
@@ -1,8 +1,25 @@
 import React from 'react'
+import { Card, Col } from 'react-bootstrap';
 
-function Doctor() {
+function Doctor({doctor}) {
+  const {img, name, age, experience, specialist} = doctor;
   return (
-    <div>Doctor</div>
+   <div >
+           <Col>
+      <Card className="mx-5">
+        <Card.Img variant="top" src={img} />
+        <Card.Body>
+          <Card.Title className="fw-bold">{name}</Card.Title>
+          <Card.Text>
+              <h5 className="fw-bold">Age: {age} Years </h5>
+              <h5 className="fw-bold">Practice Experience: {experience} Years</h5>
+              <h5 className="fw-bold">Phd Holder with specialist on {specialist} </h5>
+
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    </Col> 
+        </div>
   )
 }
 
--- src/App/components/Doctors/Doctors.js
+++ src/App/components/Doctors/Doctors.js
@@ -1,8 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Row } from 'react-bootstrap';
+import Doctor from '../Doctor/Doctor';
 
 function Doctors() {
+  const [doctors, setDoctors] = useState([]);
+    useEffect(()=>{
+        fetch(' http://localhost:3000/doctors')
+        .then(res=> res.json())
+        .then(data => setDoctors(data))
+    }, [])
+  
   return (
-    <div>Doctors</div>
+    <div className="m-4" style={{width:'90%'}}>
+            <h2 className=" my-3 text-success fw-bold fs-1">Lets Get Introduce With Our Doctors </h2>
+            <Row xs={1} md={2} >
+                {
+                    doctors.map(doctor => <Doctor doctor={doctor}></Doctor>)
+                }
+            </Row>
+        </div>
   )
 }
 
